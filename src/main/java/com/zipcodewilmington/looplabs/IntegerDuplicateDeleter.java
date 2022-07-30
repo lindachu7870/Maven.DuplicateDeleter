3ee9bcf447250acc0f18f39a8a22ebcb909926a6
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-30 13:50:22 -0400

    Finished IntegerDeleter

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -8,10 +8,7 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
     public IntegerDuplicateDeleter(Integer[] intArray) {
         super(intArray);
     }
-// TODO
-//  * Given an object, `IntegerDuplicateDeleter`, with a composite `Integer[]` object, write a method
-//  * `removeDuplicatesExactly` which removes all values in the array which occur exactly the specified number of times.
-//  * `removeDuplicates` which removes all values in the array which occur at least the specified number of times.
+
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
         int max = 0;
@@ -49,9 +46,45 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
         return result;
     }
-
+    // TODO
+    //  * Given an object, `IntegerDuplicateDeleter`, with a composite `Integer[]` object, write a method
+    //  * `removeDuplicatesExactly` which removes all values in the array which occur exactly the specified number of times.
+    //  * `removeDuplicates` which removes all values in the array which occur at least the specified number of times.
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new Integer[0];
+        int max = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+
+        int[] intMap = new int[max + 1];
+
+        // using an array to map the numbers
+        for (int i = 0; i < array.length; i++) {
+            int curr = array[i];
+            intMap[curr] = intMap[curr] + 1;
+        }
+
+        Integer[] copyArr = new Integer[array.length];
+        int newIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int curr = array[i];
+            if (intMap[curr] != exactNumberOfDuplications) {
+                copyArr[newIndex] = curr;
+                newIndex++;
+            }
+        }
+
+        Integer[] result = new Integer[newIndex];
+        for (int i = 0; i < newIndex; i++) {
+            result[i] = copyArr[i];
+        }
+
+        return result;
     }
 }
